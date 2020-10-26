0e075632fc022fb4f7e6e45780e9d2b33ba52bad
Mad <69681529+Mad880@users.noreply.github.com>
2020-10-26 14:57:03 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import discord
 from discord.ext import commands
 from keep_alive import keep_alive
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or("n!", "N!"), description="Beep boop I'm Nix's coffee machine", case_insensitive=True)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("n!", "N!"), description="DAMN BRO NO WAY IT's THE HELP SECTION!!!!!! BRUH!HH!H!H!H!!H", case_insensitive=True)
 
 @bot.event
 async def on_ready():
-    print("I am ready")
-
+    await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.playing, name="got my coffee pack"))
+    print("YUH COFFEE TIME BRUHHHHH COFFEEE MOMENT>>>>>>")
+    
 extensions = [
 	'cogs.bruh',
     'cogs.owner'
@@ -18,4 +19,4 @@ if __name__ == '__main__':
 		bot.load_extension(extension)
 
 keep_alive()
-bot.run("no u")
+bot.run("coffee-machine-token.com")
