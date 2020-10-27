fb386b84f44e54a3965f288967dc281a8e7243ff
Mad <69681529+Mad880@users.noreply.github.com>
2020-10-27 17:30:36 +0330

    UH OH NIX ADDING THINGS, RIP MY MAN BOT

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import discord
 from discord.ext import commands
 from keep_alive import keep_alive
+import os
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("n!", "N!"), description="DAMN BRO NO WAY IT's THE HELP SECTION!!!!!! BRUH!HH!H!H!H!!H", case_insensitive=True)
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.playing, name="got my coffee pack"))
+    await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.playing, name="with my nuts tbh")) #tbh
     print("YUH COFFEE TIME BRUHHHHH COFFEEE MOMENT>>>>>>")
     
 extensions = [
@@ -19,4 +20,5 @@ if __name__ == '__main__':
 		bot.load_extension(extension)
 
 keep_alive()
-bot.run("coffee-machine-token.com")
+token = os.getenv("BOT_TOKEN")
+bot.run(token)
