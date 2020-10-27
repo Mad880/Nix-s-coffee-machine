31d856d7f8fa0a7d2716a018cb4e58f665d523f1
Mad <69681529+Mad880@users.noreply.github.com>
2020-10-27 17:31:50 +0330

    yeah idk either

--- cogs/bruh.py
+++ cogs/bruh.py
@@ -21,7 +21,7 @@ class bruh(commands.Cog):
         embed.set_footer(text="dude pekis is fucking funny ngl")
         await ctx.send(embed=embed)
         #holy fucking shit i did it by myself i am so fucking good dude i am literally better than roie holy shit!!!
-
+        # holy shit didudueuu omGG SO YOOFOD AD TBEING NGOOD!
     @commands.command()
     async def duck(self, ctx):
         """it's literally n!duck what do you want"""
@@ -101,8 +101,68 @@ class bruh(commands.Cog):
     @commands.command()
     async def agree(self, ctx):
      await ctx.send("https://cdn.discordapp.com/attachments/711686111205785711/770018830181072896/DO_YOU_AGREE_WITH_HIM.mp4")
-     
+     #bRUHR RALMOA AOM Oenkey EMOENKEEY EMOENEKEY laMRAOMMOOMMAOOAMAOMMOAOMAMOA
+
+    @commands.command()
+    async def koffeemachien(self, ctx):
+      embed = discord.Embed(title="Koffee machien dispens koffee", color=0x555a74)
+      embed.set_image(url="https://cdn.discordapp.com/attachments/711686111205785711/770292166059622410/Screen_Shot_2020-09-07_at_2.20.00_AM.png")
+      embed.set_footer(text="koffee tastse e suo youg")
+      await ctx.send(embed=embed)
+#bruh i am fucking high when i make this bullshit i swear
 
 
-def setup(bot):
+    @commands.command()
+    async def suggest(self, ctx, *, suggestion):
+        channel = self.bot.get_channel(770319873804730409)
+        agree = self.bot.get_emoji(758404116367540275)
+        disagree = self.bot.get_emoji(758404182561914900)
+        embed = discord.Embed(title=f"some idiot ({ctx.author.display_name}) suggested something probably stupid", description="wow bro thanks for trying to make the bot worse i love you too", color=0x555a74) 
+        embed.add_field(name="Suggestion:", value=suggestion)
+        embed.set_footer(text="please don't be n!childporn") # bruh
+        sent = await channel.send(embed=embed)
+        await sent.add_reaction(emoji=agree)
+        await sent.add_reaction(emoji=disagree)
+        await ctx.send(f"ok cool let's see his suggestion in {channel.mention}")
+        
+    @commands.command()
+    async def nuggets(self, ctx):
+      await ctx.send("<@614140655404449945>\nI fucking HATE nuggets. He is just a complete fucking dumbass retard. nothing about him is likeable, he's just a big fucking faggot who constantly cries about getting bullied if you look at him in the wrong way. every time i see him he's off sucking a cock for 3 dollars or simping for some guy pretending to be a girl who's gonna leave him in a week. FUCK nuggets dude. everytime i wanna see him i just want to copy paste a list of torture methods that i would like to use on him, but i fear that it would get me hunted down by the cia. god fucking damnit if murder was legal i would kill him rn. I hate bitches like him. I literally can't express how much i want to kill him. If i saw him in real life that would be the last time he saw anything. Fuck him so much dude")
+
+    @commands.command()
+    async def yourmom(self, ctx):
+     embed = discord.Embed(title="your mom LOL prankt make sure to type n!prankt afterwards", color=0x555a74)
+     embed.set_image(url="https://cdn.discordapp.com/attachments/670975058734350346/770572797230710795/Screen_Shot_2020-10-27_at_8.15.20_AM.png")
+     embed.set_footer(text="nuts")
+     await ctx.send(embed=embed)
+
+    @commands.command()
+    async def heil(self, ctx):
+      embed = discord.Embed(title="Guys what the hell!?!?!?!?! it's literally hitler rn!!!!!!", color=0x555a74)
+      embed.set_image(url="https://cdn.discordapp.com/attachments/670975058734350346/770572790733602836/balls.jpg")
+      embed.set_footer(text="OMG NO WAY HITLER I'M YOUR BIGGEST FAN!!!!!!")
+      await ctx.send(embed=embed)
+
+    @commands.command()
+    async def phil(self, ctx):
+      embed = discord.Embed(title="got nutz!!!!",
+      color=0x555a74)
+      embed.set_image(url="https://cdn.discordapp.com/attachments/670975058734350346/770577878190522378/Screen_Shot_2020-10-27_at_8.25.45_AM.png")
+      embed.set_footer(text="holy shit it's literally nutshack phil")
+      await ctx.send(embed=embed)
+
+    @commands.command()
+    async def domesticviolence(self, ctx):
+      embed = discord.Embed(title="WE GOIn INTERNATIONAL!!!",
+      color=0x555a74)
+      embed.set_image(url="https://cdn.discordapp.com/attachments/670975058734350346/770572847742976000/lol.jpeg")
+      embed.set_footer(text="lmamo")
+      await ctx.send(embed=embed)
+    
+    @commands.command()
+    async def dickhead(self, ctx):
+      await ctx.send("https://cdn.discordapp.com/attachments/670975058734350346/770637879080386580/yes.png")
+
+
+def setup(bot): 
 	bot.add_cog(bruh(bot)) 
